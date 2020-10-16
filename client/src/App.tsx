cb070382a8a0062ab37bef20303d9de41373da32
Brian Riwu Kaho <b.riwukaho@gmail.com>
2020-10-17 09:15:03 +1000

    removed app.css import from app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import { Home, Login, PageNotFound } from "./Pages";
-import logo from './logo.svg';
-import './App.css';
-import { firebaseConfig } from './FireBaseConfig';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { Home, Login, PageNotFound } from './Pages';
 import * as firebase from 'firebase/app';
+import { firebaseConfig } from './FirebaseConfig';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/login" component={Login} />
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
 
-                    <Route path="/404" component={PageNotFound} />
-                    <Redirect to="/404" />
-                </Switch>
-            </Router>
-        </div>
-    );
+          <Route path="/404" component={PageNotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
