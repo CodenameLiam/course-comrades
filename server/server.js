b39366bfa9d6f4981cdb0244b642267ed3d7172a
DennisThePineapple <andennis07@gmail.com>
2020-10-17 00:39:33 +1000

    Added some basic endpoints

--- server/server.js
+++ server/server.js
@@ -12,14 +12,18 @@ const path = require("path");
 // Define application
 const app = express();
 
+// Use your firebase private key here. Initialize our firebase account, as well as our firestore databases.
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./firebase-admin-token.json");
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://uqcs-hackathon-2020.firebaseio.com"
 });
+const db = admin.firestore();
+const usersDB = db.collection("users");
+const notesDB = db.collection("notes");
+const userLikes = db.collection("userLikes");
+
 // Define port
 const port = 5000;
 
@@ -30,19 +34,61 @@ app.use(express.static("../client/build"));
 // app.use(express.static("../client/public"));
 
 
-
-
-
-// Handles Creating notes, does not handle null as an input
-app.post('/create_note', function (req,res){
+// Handles Creating notes
+app.post('/create_note', async (req, res) => {
     const name = req.query.name;
     const fileLink = req.query.fileLink;
     const courseCode = req.query.courseCode;
     const author = req.query.author;
-    const hashtags = req.query.hashtags;
-    console.log(name, fileLink, courseCode, author, hashtags[0], hashtags[2]);
-    console.log(req.body)
+    const hashtags = JSON.parse(req.query.hashtags);
+    console.log(name, fileLink, courseCode, author, hashtags);
+
+    const docRef = notesDB.doc(fileLink);
+    await docRef.set({
+        name: name,
+        fileLink: fileLink,
+        courseCode: courseCode,
+        author: author,
+        hashtags: hashtags,
+        likes: 0,
+        downloads: 0,
+        uploadDate: Date.now()
+    });
+
 })
+
+// Handles upvoting a note
+app.post('/like_note', async (req, res) => {
+    const fileLink = req.query.fileLink;
+    const username = req.query.username;
+
+    // Get current likes
+    const currentLikes = await notesDB.doc(fileLink).get().data["likes"];
+    const docRef = notesDB.doc(fileLink);
+
+    // Updates likes
+    await docRef.update({
+        likes: currentLikes + 1
+    });
+    if (!currentLikes.exists) {
+        console.log('No such document!');
+    } else {
+        console.log('Document data:', currentLikes.data());
+        console.log(x);
+    }
+})
+
+// Handles downloads
+app.post('/download_note', async(req, res) =>{
+    const fileLink = req.query.fileLink;
+    const currentDownloads = await notesDB.doc(fileLink).get().data["downloads"];
+    const docRef = notesDB.doc(fileLink);
+    // Updates downloads
+    await docRef.update({
+        downloads: currentDownloads + 1
+    });
+})
+
 
 // Server content using react clientside
 app.use((req, res) => {
