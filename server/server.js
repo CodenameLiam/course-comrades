feb8ad1d4d273e4ad7b59179b11eaa2097da23af
Liam Percy <liam.percy@roguetwo.tech>
2020-10-16 19:54:33 +1000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+// Express for server-side functionality
+const express = require("express");
+
+// Axios for API requests
+const axios = require("axios");
+
+// Path, fs and util for file handling
+// const fs = require("fs");
+// const util = require("util");
+const path = require("path");
+
+// Define application
+const app = express();
+
+// Define port
+const port = 5000;
+
+// Serve static assets built from the clientside
+app.use(express.static("../client/build"));
+
+// Serve static assets in the public folder from the clientside
+// app.use(express.static("../client/public"));
+
+// Server content using react clientside
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
