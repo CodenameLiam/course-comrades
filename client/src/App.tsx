ee7ad07acc2eb053e3852f13b34af7d29665989f
Brian Riwu Kaho <b.riwukaho@gmail.com>
2020-10-16 21:00:39 +1000

    added firebase to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { firebaseConfig } from './FireBaseConfig';
+import * as firebase from 'firebase/app';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
