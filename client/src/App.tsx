fb9d038cfcdee297872f57f86bff98709a1fdb9e
Liam Percy <liam.percy@roguetwo.tech>
2020-10-16 20:38:38 +1000

    Frontend Scaffolding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { Home, Login, PageNotFound } from "./Pages";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+
+                    <Route path="/404" component={PageNotFound} />
+                    <Redirect to="/404" />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
