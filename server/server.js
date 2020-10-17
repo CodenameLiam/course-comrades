4439f01f5194d78e6be69dfd950efa735b3f5a22
Dennis An <andennis07@gmail.com>
2020-10-17 15:10:27 +1000

    moved helper methods back to server js, moved to es modules. Change your json token into a js file.

--- server/server.js
+++ server/server.js
@@ -1,25 +1,34 @@
 // Express for server-side functionality
-const express = require('express');
-
+import express from 'express';
+// Helper functions
+//import helpers from './helpers.js';
 // Path, fs and util for file handling
 // const fs = require("fs");
 // const util = require("util");
-const path = require('path');
-
+import path from 'path';
 // Define application
 const app = express();
 
 // Use your firebase private key here. Initialize our firebase account, as well as our firestore databases.
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-admin-token.json');
+import admin from "firebase-admin";
+import serviceAccount from './firebase-admin-token.js';
 
 // Moment for handling dates
-const moment = require('moment');
+import moment from "moment";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://uqcs-hackathon-2020.firebaseio.com',
 });
+
+export const db = admin.firestore();
+const userSavedDB = db.collection('user-saved');
+const notesDB = db.collection('notes');
+const userLikesDB = db.collection('user-likes');
+const noteLikesDB = db.collection('note-likes');
+const hashtagsDB = db.collection('hashtags-notes');
+const course2NotesDB = db.collection('course-notes');
+const notes2facultyDB = db.collection('note-faculty');
 
 // Define port
 const port = 5000;
@@ -30,21 +39,23 @@ app.use(express.static('../client/build'));
 // middleware to handle requests with json body
 app.use(express.json());
 
-const db = admin.firestore();
-const usersDB = db.collection('users');
-const notesDB = db.collection('notes');
-const userLikesDB = db.collection('user-likes');
-const noteLikesDB = db.collection('note-likes');
-const hashtagsDB = db.collection('hashtags-notes');
 
 // Handles Creating notes
 app.post('/api/create_note', async (req, res) => {
+
   const name = req.body.name;
   const author = req.body.author;
   const courseCode = req.body.courseCode;
   const description = req.body.description;
   const hashtags = req.body.hashtags;
+  const faculty = req.body.faculty;
+  const semester = req.body.semester;
+
   try {
+    if (name === undefined || author === undefined || courseCode === undefined || description === undefined
+        || hashtags === undefined) {
+      throw new Error("Undefined arguments provided");
+    }
     // .add() will generate a unique id in firestore
     const note = await notesDB.add({
       name: name,
@@ -52,6 +63,8 @@ app.post('/api/create_note', async (req, res) => {
       description: description,
       courseCode: courseCode,
       hashtags: hashtags,
+      faculty: faculty,
+      semester: semester,
       likes: 0,
       downloads: 0,
       uploadDate: moment().format()
@@ -60,10 +73,13 @@ app.post('/api/create_note', async (req, res) => {
     // add an id attribute to the notes object to make it easier for the frontend
     await notesDB.doc(note.id).update({id: note.id});
 
-    hashtags.forEach(function (hashtag){
-      hashtagsDB.doc(hashtag).set({notes: admin.firestore.FieldValue.arrayUnion(note.id)}, {merge: true})
-    })
+    hashtags.forEach(function (hashtag) {
+      console.log(hashtag);
+      hashtagsDB.doc(hashtag).set({notes: db.FieldValue.arrayUnion(note.id)}, {merge: true});
+    });
 
+    await course2NotesDB.doc(courseCode).set({notes: db.FieldValue.arrayUnion(note.id)}, {merge: true});
+    await course2NotesDB.doc(courseCode).set({notes: db.FieldValue.arrayUnion(note.id)}, {merge: true});
 
   } catch (e) {
     console.log(e);
@@ -80,7 +96,7 @@ app.post('/api/like_note', async (req, res) => {
   const username = req.body.username;
   // Get current likes
   try {
-    const currentLikes = getCurrentLikes(req);
+    const currentLikes = helpers.getCurrentLikes(req);
     const noteRef = notesDB.doc(noteId);
 
     // Updates likes
@@ -91,11 +107,11 @@ app.post('/api/like_note', async (req, res) => {
     // Adds note to userlikes and like to notelikes collections
 
     await userLikesDB.doc(username).update({
-      notes: admin.firestore.FieldValue.arrayUnion(noteId)
+      notes: db.FieldValue.arrayUnion(noteId)
     })
 
     await noteLikesDB.doc(username).update({
-      users: admin.firestore.FieldValue.arrayUnion(username)
+      users: db.FieldValue.arrayUnion(username)
     })
 
     res.status(200).send('successfully liked note');
@@ -103,81 +119,155 @@ app.post('/api/like_note', async (req, res) => {
     console.log(e);
     res.status(500).send(e);
   }
-  });
+});
 
 // Handles unliking a note
-  app.post('/api/unlike_note', async (req, res) => {
-    const username = req.body.username;
-    const noteId = req.body.noteId;
-    // Get current likes
-    try {
-      const currentLikes = getCurrentLikes(req);
-      const noteRef = notesDB.doc(noteId);
+app.post('/api/unlike_note', async (req, res) => {
+  const username = req.body.username;
+  const noteId = req.body.noteId;
+  // Get current likes
+  try {
+    const currentLikes = helpers.getCurrentLikes(req);
+    const noteRef = notesDB.doc(noteId);
 
-      // Updates likes
-      await noteRef.update({
-        likes: currentLikes - 1,
-      });
+    // Updates likes
+    await noteRef.update({
+      likes: currentLikes - 1,
+    });
 
-      // Remove note to userlikes and like to notelikes collections
+    // Remove note to userlikes and like to notelikes collections
 
-      await userLikesDB.doc(username).update({
-        notes: admin.firestore.FieldValue.arrayRemove(noteId)
-      })
+    await userLikesDB.doc(username).update({
+      notes: db.FieldValue.arrayRemove(noteId)
+    })
 
-      await noteLikesDB.doc(username).update({
-        users: admin.firestore.FieldValue.arrayRemove(username)
-      })
+    await noteLikesDB.doc(username).update({
+      users: db.FieldValue.arrayRemove(username)
+    })
 
-      res.status(200).send('successfully liked note');
-    } catch (e) {
-      console.log(e);
-      res.status(500).send(e);
-    }
-
-  });
-
-// Handles downloads counter incrementing
-  app.post('/api/add_download', async (req, res) => {
-
-    const noteId = req.body.noteId;
-    try {
-      const docRef = notesDB.doc(noteId);
-      const currentDownloads = await docRef.data['downloads'];
-
-      // Updates downloads
-      await docRef.update({
-        downloads: currentDownloads + 1,
-      });
-      res.status(200).send('successfully added download');
-    } catch (e) {
-      res.status(500).send(e);
-    }
-  });
-
-// Query by hastag
-  app.get('/api/query/hashtag', async (req, res) =>{
-    try {
-
-    }
-    catch (e){
-      res.status(500).send(e);
-    }
-  })
-
-
-
-// Server content using react clientside
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-
-  var getCurrentLikes = async (req) => {
-    const noteId = req.body.noteId;
-    // Get current likes
-    const currentLikes = await notesDB.doc(noteId).get().data['likes'];
+    res.status(200).send('successfully liked note');
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
   }
 
+});
+
+// Handles downloads counter incrementing
+app.post('/api/add_download', async (req, res) => {
+
+  const noteId = req.body.noteId;
+  try {
+    const docRef = notesDB.doc(noteId);
+    const currentDownloads = await docRef.data['downloads'];
+
+    // Updates downloads
+    await docRef.update({
+      downloads: currentDownloads + 1,
+    });
+    res.status(200).send('successfully added download');
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+// // Add a saved note for a user
+// app.post('api/save-note', async (req, res) => {
+//   try {
+//
+//   } catch (e) {
+//
+//   }
+// })
+
+app.get('/api/search', async (req, res) => {
+  const faculty = req.body.faculty;
+  const hashtags = req.body.hashtags;
+  const semester = req.body.semester;
+  const courseCode = req.body.courseCode;
+  console.log(faculty, hashtags, semester, courseCode);
+  console.log(helpers.courseCodeQuery(courseCode));
+})
+
+// Server content using react clientside
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+const getNotes = async(noteIds) => {
+  var result = [];
+  noteIds.forEach( function (noteId){
+    const noteData = notesDB.doc(noteId).get().data();
+    result.push(
+        {
+          name: noteData['name'],
+          author: noteData['author'],
+          description: noteData['description'],
+          courseCode: noteData['courseCode'],
+          hashtags: noteData['hashtags'],
+          likes: noteData['likes'],
+          downloads: noteData['downloads'],
+          uploadDate: noteData['uploadDate'],
+          noteId : noteId
+        }
+    );
+  })
+  return result;
+}
+
+const getCurrentLikes = async (req) => {
+  const noteId = req.body.noteId;
+  // Get current likes
+  return  await notesDB.doc(noteId).get().data['likes'];
+}
+
+// Query by hashtag
+const hashtagQuery =  async (hashtags) => {
+  try {
+    var noteIds = [];
+    hashtags.forEach((hashtag) => {
+      const notes = (hashtagsDB.doc(hashtag).get()).data()['notes']
+      notes.forEach((noteID) => {
+        noteIds.push(noteID);
+      })
+    })
+    return noteIds;
+  } catch (e) {
+
+  }
+};
+
+const courseCodeQuery = async (courseCode) => {
+  try {
+    var noteIds = [];
+    var notes = await course2NotesDB.doc(courseCode).get().data()['notes']
+    return noteIds;
+  }
+  catch (e){
+  }
+}
+
+// Filter by semester, takes in array of notes, not id's
+const semesterQuery = async (semester, notes) =>{
+  const results = [];
+  notes.forEach((note)=>{
+    if (note.semester === semester){
+      results.push(note);
+    }
+  })
+  return results;
+}
+
+// Filter by faculty, takes in array of notes, not id's
+const facultyQuery = async (query, notes) =>{
+  const results = [];
+  notes.forEach((note)=>{
+    if (note.query === query){
+      results.push(note);
+    }
+  })
+  return results;
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
